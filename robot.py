ce14532196c223f9d6201ca76d8c50ab9bd5a976
Dojiawesome2000 <145794573+Dojiawesome2000@users.noreply.github.com>
2023-11-30 15:42:20 -0600

    asdlf;kj

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
 from wpilib import *
+import wpilib.drive
 from wpimath import *
 from wpimath.controller import *
 from wpimath.estimator import *
@@ -7,7 +8,7 @@ from wpimath.geometry import *
 from wpimath.interpolation import *
 from wpimath.kinematics import *
 from wpimath.spline import *
-from wpimath.system import *
+# from wpimath.system import *
 from wpimath.trajectory import *
 from wpimath.units import *
 from wpinet import *
@@ -28,16 +29,39 @@ from rev import *
 
 class Robot(TimedRobot):
     def robotInit(self):
-        self.left_motor = wpilib.TalonSRX(0)
-        self.right_motor = wpilib.TalonSRX(1)
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.frontleft_motor = PWMTalonSRX(0)
+        self.frontright_motor = PWMTalonSRX(2)
+        self.backleft_motor = PWMTalonSRX(1)
+        self.backright_motor = PWMTalonSRX(3)
+
+        self.left = MotorControllerGroup(self.frontleft_motor, self.backleft_motor)
+        self.right = MotorControllerGroup(self.frontright_motor, self.backright_motor)
+        
+        self.drive = wpilib.drive.DifferentialDrive(self.left, self.right)
+        self.drive.setExpiration(0.1)
+
+        self.controller = XboxController(0)
+
+
+
     def robotPeriodic(self): pass
 
     def autonomousInit(self): pass
     def autonomousPeriodic(self): pass
     def autonomousExit(self): pass
 
-    def teleopInit(self): pass
-    def teleopPeriodic(self): pass    
+    def teleopInit(self):
+        """Executed at the start of teleop mode"""
+        self.drive.setSafetyEnabled(True)
+    def teleopPeriodic(self):
+        """Runs the motors with tank steering"""
+        self.drive.tankDrive(self.controller.getLeftY * -1, self.controller.getRightY)
+        SmartDashboard.putNumber("TalonSRX front left motor power", self.frontleft_motor.get())
+        SmartDashboard.updateValues()
     def teleopExit(self): pass
 
     def _simulationInit(self): pass
